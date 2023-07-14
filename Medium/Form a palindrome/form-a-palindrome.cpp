63d411aeabd354b9c4d496d17ece27aba8efdccf
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-07-14 13:39:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Form a palindrome/form-a-palindrome.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMinInsertions(string str)
+    {
+        string t=str;
+        int n=t.length();
+        int start=0;
+        int end=n-1;
+        while(start<end)
+        {
+            swap(t[start],t[end]);
+            start++;
+            end--;
+        }
+        // cout<<t<<" "<<str<<endl;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++)
+            {
+                if(str[i-1]==t[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.findMinInsertions(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
