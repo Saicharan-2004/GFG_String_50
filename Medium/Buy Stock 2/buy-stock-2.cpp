447011ed3b5f62f6d41adab1eebe9ca9f01aa810
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2023-07-15 21:19:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Stock 2/buy-stock-2.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&prices, int n) 
+    {
+        int profit=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(prices[i]<prices[i+1])
+            {
+                profit+=prices[i+1]-prices[i];
+            }
+        }
+        return profit;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
